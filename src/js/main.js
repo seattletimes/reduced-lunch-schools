0d1d89c38e9bc754a3ff34ead133fa295e1ccade
emilymeng <eeng@seattletimes.com>
2019-01-17 17:08:49 -0800

    both maps working

--- src/js/main.js
+++ src/js/main.js
@@ -76,11 +76,75 @@ if (mapElement) {
     };
     return s;
   }
-  
-	var data = require("./reduced-districts.geo.json");
+
+    var geojson = L.geoJson(data, {
+    style: style,
+    onEachFeature: onEachFeature
+  }).addTo(map);
+
+}
+
+	var data2 = require("./championship_school.geo.json");
   var mapElement = document.querySelector("leaflet-map.champs");
 
-  var geojson = L.geoJson(data, {
+  if (mapElement) {
+  var L = mapElement.leaflet;
+  var map = mapElement.map;
+
+  map.scrollWheelZoom.disable();
+
+  var focused = false;
+
+  var champ = "championships";
+
+  data2.features.forEach(function(f) {
+	});
+
+	var onEachFeature = function(feature, layer) {
+  layer.bindPopup(ich.popup(feature.properties))
+
+  layer.on({
+  	     mouseover: function(e) {
+        layer.setStyle({ weight: 3, fillOpacity: .8 });
+      },
+      mouseout: function(e) {
+        if (focused && focused == layer) { return }
+        layer.setStyle({ weight: 2, fillOpacity: 0.5 });
+      }
+    });
+	};
+
+	var getColor2 = function(d) {
+    var value = d[champ];
+    if (typeof value == "string") {
+      value = Number(value.replace(/,/, ""));
+    }
+    // console.log(value)
+    if (typeof value != "undefined") {
+      // condition ? if-true : if-false;
+     return value >= 10 ? '#6e016b' :
+     		value >= 7 ? '#d88419d' :
+     		value >= 4 ? '#e8c6bb1' :
+        value >= 1 ? '#9ebcda' :
+             
+             '#edf8fb' ;
+    } else {
+      return "gray"
+    }
+  };
+
+  var style = function(feature) {
+    var s = {
+      fillColor: getColor2(feature.properties),
+      weight: 2,
+      opacity: .1,
+      color: '#000',
+      fillOpacity: 0.5
+    };
+    return s;
+  }
+
+  var geojson = L.geoJson(data2, {
     style: style,
     onEachFeature: onEachFeature
   }).addTo(map);
